3dbb71c8b3a089323b2002332e598145d091acba
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-02-25 09:00:02 +0000

    creating the Grid components without styles just redering it using map() function to iterate over the movie array

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //components
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
 
 //Hooks
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -24,6 +25,11 @@ const Home = () => {
                text={state.results[0].overview}
             />
          ) : null}
+         <Grid header="Popular Movies">
+            {state.results.map((movie) => (
+               <div key={movie.id}>{movie.title}</div>
+            ))}
+         </Grid>
       </>
    );
 };
