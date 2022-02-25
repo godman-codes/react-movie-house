4fa769b0c91322c4ab75f743d7f592a03fe89585
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-02-25 02:15:19 +0000

    creating and styling hero image the next components after the header

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //components
+import HeroImage from "./HeroImage";
 
 //Hooks
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -14,7 +15,17 @@ const Home = () => {
 
    console.log(state);
 
-   return <div>Home Page</div>;
+   return (
+      <>
+         {state.results[0] ? (
+            <HeroImage
+               image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+               title={state.results[0].original_title}
+               text={state.results[0].overview}
+            />
+         ) : null}
+      </>
+   );
 };
 
 export default Home;
