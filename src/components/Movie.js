5a6ac50596754d555240c3a226db56f6c177d165
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-02-28 14:53:09 +0000

    fetching the movie page data using the individual movie id as the query parameter

---
+++ src/components/Movie.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+// config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+// COMPONENTS
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+//hooks
+import { useMovieFetch } from "../hooks/useMovieFetch";
+//images
+import NoImage from "../images/no_image.jpg";
+const Movie = () => {
+   const { movieId } = useParams();
+   const { state: movie, loading, error } = useMovieFetch(movieId);
+   console.log(movie);
+   return (
+      <>
+         <div>Movie</div>
+      </>
+   );
+};
+
+export default Movie;
