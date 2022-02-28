f4a1db01072068e70fda4131e066962e20f3f5ec
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-02-28 23:36:06 +0000

    creating the actor component to render individual actors the put it in a grid for some structure

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,13 +8,16 @@ import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 //hooks
 import { useMovieFetch } from "../hooks/useMovieFetch";
 //images
 import NoImage from "../images/no_image.jpg";
+import Thumb from "./Thumb";
 const Movie = () => {
    const { movieId } = useParams();
    const { state: movie, loading, error } = useMovieFetch(movieId);
+   console.log(movie);
    if (loading) return <Spinner />;
    if (error) return <div>Something went wrong...</div>;
    return (
@@ -26,6 +29,20 @@ const Movie = () => {
             budget={movie.budget}
             revenue={movie.revenue}
          />
+         <Grid header="Actors">
+            {movie.actors.map((actor) => (
+               <Actor
+                  key={actor.credit_id}
+                  name={actor.name}
+                  character={actor.character}
+                  imageUrl={
+                     actor.profile_path
+                        ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                        : NoImage
+                  }
+               />
+            ))}
+         </Grid>
       </>
    );
 };
