380894501e14ad3a6fe48eee6144d15abce9a320
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-03-05 08:12:26 +0000

    creating the login page and autheticating it form the movie db api

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Movie from "./components/Movie";
 import NotFound from "./components/Notfound";
+import Login from "./components/Login";
+//Context
+import UserProvider from "./Context";
 // styles
 import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => (
    <Router>
-      <Header />
-      <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/:movieId" element={<Movie />} />
-         <Route path="/*" element={<NotFound />} />
-      </Routes>
-      <GlobalStyle />
+      <UserProvider>
+         <Header />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/:movieId" element={<Movie />} />
+            <Route path="/*" element={<NotFound />} />
+         </Routes>
+         <GlobalStyle />
+      </UserProvider>
    </Router>
 );
 export default App;
